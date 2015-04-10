70ad7a05c6afb76a6d87bdad17cea57387151a0d
gguerrero11 <gguerrero11@gmail.com>
2015-04-10 17:07:05 -0600

    adds HitChecker

---
+++ MapTest/HitChecker.h
@@ -0,0 +1,13 @@
+//
+//  HitChecker.h
+//  MapTest
+//
+//  Created by Gabriel Guerrero on 4/10/15.
+//  Copyright (c) 2015 Gabe Guerrero. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HitChecker : NSObject
+
+@end
---
+++ MapTest/HitChecker.m
@@ -0,0 +1,13 @@
+//
+//  HitChecker.m
+//  MapTest
+//
+//  Created by Gabriel Guerrero on 4/10/15.
+//  Copyright (c) 2015 Gabe Guerrero. All rights reserved.
+//
+
+#import "HitChecker.h"
+
+@implementation HitChecker
+
+@end
